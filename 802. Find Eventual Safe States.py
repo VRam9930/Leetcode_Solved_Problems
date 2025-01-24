31395dfa2e5eb249acd2f29515da0afba43e1386
VRam9930 <138971563+VRam9930@users.noreply.github.com>
2025-01-24 22:27:11 +0530

    Topics:Depth-First Search, Breadth-First Search, Graph ,Topological Sort

---
+++ 802. Find Eventual Safe States.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def eventualSafeNodes(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[int]
+        """
+       
+        n = len(graph)
+        res = []
+        visited = set()
+        memo = [0] * n
+
+        def dfs(i):
+            if memo[i] == 1 or len(graph[i]) == 0:
+                return True
+            elif memo[i] == -1 or i in visited:
+                return False
+            
+            visited.add(i)
+            
+            for neighbour in graph[i]:
+                if not dfs(neighbour):
+                    memo[i] = -1
+                    return False
+
+            memo[i] = 1
+            return True
+        
+        for i in range(n):
+            if dfs(i):
+                res.append(i)
+        
+        return res
+
+        
